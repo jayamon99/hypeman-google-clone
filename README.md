d15b4664f5e0fec70c325486e2bec6d8265e8deb
Quaye Henry <henryquaye99@outlook.com>
2021-11-07 20:58:09 +0000

    Project files

---
+++ README.md
@@ -0,0 +1,3 @@
+# SideHustle_HYPEMAN
+
+## Group Project
---
+++ hypeman/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import Body from './Components/Body/Body';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+
+
+
+function App() {
+  return (
+   <>
+    <Header />
+    <Body />
+    <Footer />
+   </>
+  );
+}
+
+export default App;
